c17907e65c453e7ec2e04363a0224c94ae8eca09
Rritvik <rritvik.kakkar16@gmail.com>
2024-09-21 20:13:32 +0530

    API Keys changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'b316ec3438mshd4570f8414ab4f0p103e88jsn9de9d208a6a1',
+		'x-rapidapi-key': // RAPID API KEY,
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,4 +10,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = '8a142981ffdf78fe45603c45de50b156';
+export const WEATHER_API_KEY = // WEATHER API KEY;
