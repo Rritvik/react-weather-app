db98b8287b648e87e667a2e816c5a0935a1309f0
Rritvik <rritvik.kakkar16@gmail.com>
2024-09-21 19:56:08 +0530

    React Weather App

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b316ec3438mshd4570f8414ab4f0p103e88jsn9de9d208a6a1',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '8a142981ffdf78fe45603c45de50b156';
